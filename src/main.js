34e4cb93cc98ceca037f99e289c2713e70668458
WillianFarias <williansousafarias@gmail.com>
2020-10-14 22:44:01 -0300

    msg erro e loading

--- src/main.js
+++ src/main.js
@@ -16,6 +16,18 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
+  setLoading(loading = true) {
+    if (loading === true) {
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode('Carregando'));
+      loadingEl.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl);
+    } else {
+      document.getElementById('loading').remove();
+    }
+  }
+
   async addRepository(event) {
     event.preventDefault();
 
@@ -24,23 +36,28 @@ class App {
     if (repoInput.length === 0)
       return;
 
-    const response = await api.get(`/repos/${repoInput}`);
+    this.setLoading();
 
-    console.log(response);
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
 
-    //desestruturacao
-    const { name, description, html_url, owner: { avatar_url }} = response.data;
+      //desestruturacao
+      const { name, description, html_url, owner: { avatar_url }} = response.data;
 
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url
-    });
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url
+      });
+
+      this.render();
+    } catch (err) {
+      alert('O repositório não existe!');
+    }
 
     this.inputEl.value = '';
-
-    this.render();
+    this.setLoading(false);
   }
 
   render() {
